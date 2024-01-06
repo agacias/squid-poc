8c46816a48eced56b913d974de7f13e50d41a5ca
Alberto Gacías <alberto@migasfree.org>
2024-01-06 09:49:52 +0100

    variables

--- registry-pwd.sh
+++ registry-pwd.sh
@@ -11,12 +11,8 @@
 #     docker push registry.red.zaragoza.es:5000/nginx
 #     docker pull registry.red.zaragoza.es:5000/nginx
 
+source ./env
 
-DNS=registry.red.zaragoza.es
-USERNAME="play"
-PASSWORD="docker"
-PATH_REGISTRY=/registry
-IP_REGISTRY=$(hostname -i)
 
 mkdir -p ${PATH_REGISTRY}/data || :
 mkdir -p ${PATH_REGISTRY}/certs || :
@@ -27,21 +23,21 @@ cd certs
 
 # Create a CA crt
 
-openssl req -new -x509 -nodes -newkey rsa:4096 -extensions v3_ca -sha256 -days 3650 -subj "/C=ES/ST=ZARAGOZA/L=ZARAGOZA/O=migasfree/CN=${DNS}" -keyout ca.key -out ca.crt
+openssl req -new -x509 -nodes -newkey rsa:4096 -extensions v3_ca -sha256 -days 3650 -subj "/C=ES/ST=ZARAGOZA/L=ZARAGOZA/O=migasfree/CN=${REGISTRY}" -keyout ca.key -out ca.crt
 chmod 600 ca.key
 
 # Create a CSR:
-openssl req -newkey rsa:2048 -nodes -sha256 -keyout ${DNS}.key -out ${DNS}.csr -subj "/C=ES/ST=ZARAGOZA/L=ZARAGOZA/O=migasfree/OU=Core/CN=${DNS}"
+openssl req -newkey rsa:2048 -nodes -sha256 -keyout ${REGISTRY}.key -out ${REGISTRY}.csr -subj "/C=ES/ST=ZARAGOZA/L=ZARAGOZA/O=migasfree/OU=Core/CN=${REGISTRY}"
 
 # Check contents of CSR (optional):
-# openssl req -in ${DNS}.csr -text -noout
+# openssl req -in ${REGISTRY}.csr -text -noout
 
 # Sign the CSR, resulting in CRT and add the v3 SAN extension:
-openssl x509 -req -in ${DNS}.csr -out ${DNS}.crt -CA ca.crt -CAkey ca.key -CAcreateserial -sha256 -days 1095 -extensions SAN -extfile <(cat  /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=@san_names\nbasicConstraints=CA:FALSE\nkeyUsage=nonRepudiation,digitalSignature,keyEncipherment\n[san_names]\nDNS.1=${DNS}\n"))
-chmod 600 ${DNS}.key
+openssl x509 -req -in ${REGISTRY}.csr -out ${REGISTRY}.crt -CA ca.crt -CAkey ca.key -CAcreateserial -sha256 -days 1095 -extensions SAN -extfile <(cat  /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=@san_names\nbasicConstraints=CA:FALSE\nkeyUsage=nonRepudiation,digitalSignature,keyEncipherment\n[san_names]\nDNS.1=${REGISTRY}\n"))
+chmod 600 ${REGISTRY}.key
 
 # Check contents of CRT (optional)
-#openssl x509 -in ${DNS}.crt -text -noout
+#openssl x509 -in ${REGISTRY}.crt -text -noout
 
 cd ..
 
@@ -55,10 +51,10 @@ echo "${PASSWORD}" | sudo htpasswd -iB ./certs/htpasswd ${USERNAME}
 
 
 # Copiamos el certificado de la CA en docker
-mkdir -p /etc/docker/certs.d/${DNS}:5000/
-cp certs/ca.crt /etc/docker/certs.d/${DNS}:5000/
+mkdir -p /etc/docker/certs.d/${REGISTRY}:5000/
+cp certs/ca.crt /etc/docker/certs.d/${REGISTRY}:5000/
 
-docker run -d -p 5000:5000 --name registry -v $(pwd)/data:/var/lib/registry -v $(pwd)/certs:/etc/security -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/security/${DNS}.crt -e REGISTRY_HTTP_TLS_KEY=/etc/security/${DNS}.key -e REGISTRY_AUTH=htpasswd -e REGISTRY_AUTH_HTPASSWD_PATH=/etc/security/htpasswd -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" --restart always registry:2
+docker run -d -p 5000:5000 --name registry -v $(pwd)/data:/var/lib/registry -v $(pwd)/certs:/etc/security -e REGISTRY_HTTP_TLS_CERTIFICATE=/etc/security/${REGISTRY}.crt -e REGISTRY_HTTP_TLS_KEY=/etc/security/${REGISTRY}.key -e REGISTRY_AUTH=htpasswd -e REGISTRY_AUTH_HTPASSWD_PATH=/etc/security/htpasswd -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" --restart always registry:2
 
 sleep 5
 
@@ -67,7 +63,7 @@ for NODE in $(docker node ls -q)
 do
     IP=$(docker node inspect ${NODE} |jq -r .[].Status.Addr)
     scp -o "StrictHostKeyChecking no" certs/ca.crt root@${IP}:/usr/local/share/ca-certificates/
-    ssh root@${IP} "update-ca-certificates;echo "${IP_REGISTRY} ${DNS}" >> /etc/hosts ; echo  ${PASSWORD} | docker login ${DNS}:5000 --username ${USERNAME} --password-stdin"
+    ssh root@${IP} "update-ca-certificates;echo "${IP_REGISTRY} ${REGISTRY}" >> /etc/hosts ; echo  ${PASSWORD} | docker login ${REGISTRY}:5000 --username ${USERNAME} --password-stdin"
 done
 
 
